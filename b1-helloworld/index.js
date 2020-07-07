1769cad6e8afc5645a332795ccc0c8a3bb84a057
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-07-07 22:54:26 +0700

    L2: Template Engine with Pug

--- b1-helloworld/index.js
+++ b1-helloworld/index.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', function (req, res) {
+    // param1: path to pug template file: index.pug
+    // param2: variable for HTML is object include key-value
+    res.render('index', {
+        name: 'AAA'
+    });
+});
+
+app.get('/users', function (req, res) {
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Nguyen' },
+            { id: 2, name: 'Phu' },
+            { id: 3, name: 'Quang' }
+        ]
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
