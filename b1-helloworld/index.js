a8a52c4dc07896ba72529d04890a3ed5496115ef
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-07-07 23:49:12 +0700

    L3: Query Parameter with Search form

--- b1-helloworld/index.js
+++ b1-helloworld/index.js
@@ -5,6 +5,12 @@ const port = 3000
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+    { id: 1, name: 'Nguyen' },
+    { id: 2, name: 'Phu' },
+    { id: 3, name: 'Quang' }
+]
+
 app.get('/', function (req, res) {
     // param1: path to pug template file: index.pug
     // param2: variable for HTML is object include key-value
@@ -15,11 +21,20 @@ app.get('/', function (req, res) {
 
 app.get('/users', function (req, res) {
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Nguyen' },
-            { id: 2, name: 'Phu' },
-            { id: 3, name: 'Quang' }
-        ]
+        users: users
+    });
+});
+
+
+app.get('/users/search', function (req, res) {
+    //console.log(req.query);
+    var q = req.query.q;
+    var matchUsers = users.filter(function (user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+
+    res.render('users/index', {
+        users: matchUsers
     });
 });
 
